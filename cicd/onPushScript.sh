c3f0415e4c94ba4a0ac4d976a11c87f706c19456
wuyh28 <wuyh28@xiaopeng.com>
2026-02-13 18:12:41 +0800

    chore(script): 优化onPushScript.sh流程，调整opencode-ai安装与执行顺序，新增代码提交检查；修正setup.bat脚本语法错误

--- cicd/onPushScript.sh
+++ cicd/onPushScript.sh
@@ -6,10 +6,6 @@ echo 'on push  script'
 
 echo 'installing opencode-ai'
 
-npm install -g opencode-ai
-
-echo 'set api key'
-
 export OPENCODE_API_KEY="sk-sp-de6e887faf1f49acbad120766050fdd9"
 
 mkdir -p ~/.config/opencode
@@ -17,9 +13,12 @@ cp ./cicd/opencode.json ~/.config/opencode/opencode.json
 
 echo 'install opencode-ai finish'
 
-
 opencode run '你好' --agent plan --model CodingPlan/qwen3-max-2026-01-23
 
+echo 'check opencode-ai finish'
 
-echo 'run opencode-ai finish'
+echo 'start check commit'
+
+opencode run '检查刚刚提交的代码' --agent plan --model CodingPlan/qwen3-max-2026-01-23
+
 
